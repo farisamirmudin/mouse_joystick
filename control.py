3f2e99eacadc6ae833e1033738b9ffcabc275cb5
faris <faris071298@gmail.com>
2024-02-15 22:16:03 +0800

    Add JOYDEVICEADDED event

--- control.py
+++ control.py
@@ -4,31 +4,24 @@ import pyautogui as ag
 
 # pygame setup
 pygame.init()
-
-# joystick setup
-joysticks = [pygame.joystick.Joystick(x) for x in range(pygame.joystick.get_count())]
-joystick = joysticks[0]
+pygame.joystick.init()
 
 def main():
+    joysticks = {}
     done = False
-    multiplier_move = 4
+    multiplier_move = 10
     multiplier_scroll = 2
-    xOffset = 0
-    yOffset = 0
-    scroll_delta = 0
-    scaleMove = lambda x, k: x*k if abs(x)>0.1 else 0
-    get_axis = joystick.get_axis
-    volumeup = False
-    volumedown = False
+    scale = lambda x, k: x*k if abs(x)>0.1 else 0
+    clock = pygame.time.Clock()
 
     while not done:
-        if volumedown:
-            ag.press(u'KEYTYPE_SOUND_DOWN')
-            volumedown = False
-        if volumeup:
-            ag.press(u'KEYTYPE_SOUND_UP')
-            volumeup = False
+        clock.tick(120)
         for event in pygame.event.get():
+            if event.type == pygame.JOYDEVICEADDED:
+                joy = pygame.joystick.Joystick(event.device_index)
+                joysticks[joy.get_instance_id()] = joy
+            if event.type == pygame.JOYDEVICEREMOVED:
+                del joysticks[event.instance_id]
             if event.type == pygame.JOYBUTTONDOWN:
                 if event.button == 0:
                     mouse.click(mouse.LEFT)
@@ -49,20 +42,23 @@ def main():
             if event.type == pygame.JOYBUTTONUP:
                 if event.button == 12:
                     ag.keyUp('command')
-            if event.type == pygame.JOYAXISMOTION:
-                xOffset = scaleMove(get_axis(0), multiplier_move)
-                yOffset = scaleMove(get_axis(1), multiplier_move)
-                scroll_delta = scaleMove(get_axis(3), multiplier_scroll)
 
-                # https://stackoverflow.com/questions/64641029/trying-to-mute-the-system-using-pyautogui-pressvolumemute-but-its-not-doing
+        for joystick in joysticks.values():
+            xOffset = scale(joystick.get_axis(0), multiplier_move)
+            yOffset = scale(joystick.get_axis(1), multiplier_move)
+            scroll_delta = scale(joystick.get_axis(3), multiplier_scroll)
+            mouse.move(xOffset, yOffset, False)
+            mouse.wheel(scroll_delta)
 
-                if get_axis(4) > 0.999:
-                    volumedown = True
-                if get_axis(5) > 0.999:
-                    volumeup = True
-
-                mouse.move(xOffset, yOffset, False)
-                mouse.wheel(scroll_delta)
+            if joystick.get_axis(2) > 0.99:
+                ag.press('right')
+            if joystick.get_axis(2) < -0.99:
+                ag.press('left')
+            # https://stackoverflow.com/questions/64641029/trying-to-mute-the-system-using-pyautogui-pressvolumemute-but-its-not-doing
+            if joystick.get_axis(4) > 0.99:
+                ag.press(u'KEYTYPE_SOUND_DOWN')
+            if joystick.get_axis(5) > 0.99:
+                ag.press(u'KEYTYPE_SOUND_UP')
 
 if __name__ == "__main__":
     main()
